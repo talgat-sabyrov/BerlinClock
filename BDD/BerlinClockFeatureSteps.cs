39cac65672f88646a5d2413691819c2bd2539847
SabyrovT <sabyrov.talgat@gmail.com>
2020-06-08 11:39:13 +0600

    BDD changed according to new logic

--- BDD/BerlinClockFeatureSteps.cs
+++ BDD/BerlinClockFeatureSteps.cs
@@ -2,26 +2,30 @@
 using TechTalk.SpecFlow;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Linq;
+using BerlinClock.Abstract;
+using BerlinClock.BusinessLogic;
 
 namespace BerlinClock
 {
     [Binding]
     public class TheBerlinClockSteps
     {
-        private ITimeConverter berlinClock = new TimeConverter();
+        private TimeConverterFactory factory = new TimeConverterFactory();
+        private TimeConverterBase berlinClock;
         private String theTime;
 
         
         [When(@"the time is ""(.*)""")]
         public void WhenTheTimeIs(string time)
         {
+            berlinClock = factory.Create();
             theTime = time;
         }
         
         [Then(@"the clock should look like")]
         public void ThenTheClockShouldLookLike(string theExpectedBerlinClockOutput)
         {
-            Assert.AreEqual(berlinClock.convertTime(theTime), theExpectedBerlinClockOutput);
+            Assert.AreEqual(berlinClock.Convert(theTime), theExpectedBerlinClockOutput);
         }
 
     }
