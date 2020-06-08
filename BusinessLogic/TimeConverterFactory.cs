a65b60a3c80bb2e7651a7b75262dbd9df992013c
SabyrovT <sabyrov.talgat@gmail.com>
2020-06-08 11:38:55 +0600

    TimeConverterFactory created

---
+++ BusinessLogic/TimeConverterFactory.cs
@@ -0,0 +1,14 @@
+﻿namespace BerlinClock.BusinessLogic
+{
+    public class TimeConverterFactory
+    {
+        public TimeConverter Create()
+        {
+            return new TimeConverter(new SecondsRow(),
+                                     new FiveHoursRow(),
+                                     new OneHoursRow(),
+                                     new FiveMinutesRow(),
+                                     new OneMinutesRow());
+        }
+    }
+}
